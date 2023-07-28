c3c128d7ecf25a64726afd49f577709fe8cb930c
Marija-Sokolets <128020732+Marija-Sokolets@users.noreply.github.com>
2023-07-28 11:13:04 +0300

    js-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map((url) =>
+    ` <li class="gallery__item">
+        <a class="gallery__link" href=${url.original}">
+          <img
+            class="gallery__image"
+            src="${url.preview}"
+            data-source="${url.original}"
+            alt="${url.description}"
+          />
+        </a>
+      </li>`
+).join("");
+
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+gallery.addEventListener("click", galleryItemClick);
+
+function galleryItemClick(event) {
+    event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+  <img src="${event.target.dataset.source}" width="800", height="600">
+  `);
+
+  instance.show();
+
+  gallery.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  });
+};
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) =>
+    `<li class= "gallery_item">
+        <a class="gallery_link" href="${original}">
+            <img class="gallery_image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+).join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// galleryList.addEventListener("click", event => {
+    const SimpleLightbox (".gallery a", {
+        // captions: true,
+        // captionsType: "attr",
+        captionsData: "alt",
+        // captionPosition: "bottom", 
+        captionDelay: 250,
+        // download: false,
+    });
+// });
